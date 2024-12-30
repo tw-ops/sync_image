f6837b9adadf2d55518e29100f951c9ce293016b
tw-ops <126220535+tw-ops@users.noreply.github.com>
2024-12-30 11:21:17 +0800

    Update main.go

--- main.go
+++ main.go
@@ -291,9 +291,8 @@ func execCmd(command string, args ...string) (string, error) {
 	output := out.String()
 
 	if err != nil {
-		return output, fmt.Errorf("执行命令时发生错误: '%w', 命令: '%s %s', 输出: '%s'", err, command, strings.Join(args, " "), output)
+	    return output, fmt.Errorf("执行命令时发生错误: '%w'\n命令:\n```\n%s %s\n```\n输出:\n```\n%s\n```\n", err, command, strings.Join(args, " "), output)
 	}
-
 	return output, nil
 }
 
